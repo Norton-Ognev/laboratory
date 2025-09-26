25b3ece0755d5abe5aeaab0f1bd7e7b4b2528fed
Norton <der9004@gmail.com>
2025-09-26 23:33:30 +0300

    Update lab0.c

--- lab0.c
+++ lab0.c
@@ -4,23 +4,32 @@
 int main() {
     float results[3][3];
 
-    for (int x = 1; x <= 3; x++) {
-        for (int a = 2; a <= 4; a++) {
-            float z = fmin(sqrt(tan((a-x)/(float)x)), fmin(log((a*a)/(float)x), atan((x*x*x*sqrt(a))/(a+2.5))));
-            results[x-1][a-2] = z;
+    for (float x = 1; x <= 3; x=x+1.3) {
+        for (float a = 2; a <= 4; a=a+1.3) {
+            float z = fmin(sqrt(tan((a-x)/x)), fmin(log((a*a)/x), atan((x*x*x*sqrt(a))/(a+2.5))));
+            results[ (int)x-1][(int)a-2] = z;
         }
     }
 
-    printf("     ");
-    for (int a = 2; a <= 4; a++) {
-        printf("a=%-8d", a);
+    printf("         ");
+    for (float a = 2; a <= 4; a=a+1.3) {
+        if (a == (int)a) {
+            printf("a=%-6d", (int)a);
+        } else {
+            printf("a=%-6.1f", a);
+        }
     }
+    
     printf("\n");
 
-    for (int x = 1; x <= 3; x++) {
-        printf("x=%d", x);
-        for (int a = 2; a <= 4; a++) {
-            printf(" %-8f", results[x-1][a-2]);
+    for (float x = 1; x <= 3; x=x+1.3) {
+        if (x == (int)x) {
+            printf("x=%-3d", (int)x);
+        } else {
+            printf("x=%-2.1f", x);
+        }
+        for (float a = 2; a <= 4; a=a+1.3) {
+            printf(" %-8f", results[(int)x-1][(int)a-2]);
         }
         printf("\n");
     }
